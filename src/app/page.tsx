a7d369d942c17130e40e8f635f2f57dd53d89365
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-15 10:28:22 -0500

    Add Layout and NavBar into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import * as React from "react";
-import { NextUIProvider } from '@nextui-org/react'
-import Image from 'next/image'
+import NavBar from '../../components/common-components/nav-bar';
+import Layout from "../../components/common-components/layout";
 
 export default function Home() {
   return (
-    <NextUIProvider className="bg-white text-black">
-      <h1>
-        NextJS Project
-      </h1>
-    </NextUIProvider>
+    <Layout>
+      <NavBar />
+    </Layout>
   )
 }
