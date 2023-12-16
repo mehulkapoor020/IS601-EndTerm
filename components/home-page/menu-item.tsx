1c565733959f06bda7fe58d73e4d20e16cd5553a
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-16 12:50:15 -0500

    Add Menu component closes RunjeethNikam#32

--- components/home-page/menu-item.tsx
+++ components/home-page/menu-item.tsx
@@ -1,28 +1,28 @@
 import { Card, CardBody, CardFooter } from '@nextui-org/react';
 import Image from 'next/image';
 
-type Menu = {
+export type MenuProps = {
   image: string;
   name: string;
   price: string;
 };
 
-export default function Menu(menu: Menu) {
+export default function MenuItem({ menu }: { menu: MenuProps }) {
   return (
-    <Card className='max-w-sm lg:max-w-xl bg-warning-500 data-[hover]:bg-warning-300' shadow="lg" key={index} isHoverable>
-            <CardBody className="">
-              <Image
-                alt="name"
-                className="w-full object-cover"
-                src={menu.image}
-                width={365}
-                height={342}
-              />
-            </CardBody>
-            <CardFooter className="md:text-lg justify-between">
-              <b>{menu.name}</b>
-              <p className="text-default-500">{menu.price}</p>
-            </CardFooter>
-          </Card>
+    <Card className='max-w-sm lg:max-w-xl bg-warning-500 data-[hover]:bg-warning-300' shadow="lg" isHoverable>
+      <CardBody className="">
+        <Image
+          alt="name"
+          className="w-full object-cover"
+          src={menu.image}
+          width={365}
+          height={342}
+        />
+      </CardBody>
+      <CardFooter className="md:text-lg justify-between">
+        <b>{menu.name}</b>
+        <p className="text-default-500">{menu.price}</p>
+      </CardFooter>
+    </Card>
   );
 }
