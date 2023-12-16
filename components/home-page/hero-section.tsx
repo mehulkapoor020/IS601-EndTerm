ce7bfaaa0a2ff79eed78bada294e68771f982836
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-15 22:07:51 -0500

    Add responsiveness closes RunjeethNikam#26

--- components/home-page/hero-section.tsx
+++ components/home-page/hero-section.tsx
@@ -16,13 +16,13 @@ export default function HeroSection() {
       <div className='w-full text-white absolute z-10 h-full flex flex-col justify-center items-center gap-20'>
         <div>
           <p
-            className='w-fit text-3xl font-bold text-center leading-tight'
+            className='w-fit text-3xl sm:text-7xl font-bold text-center leading-tight'
           >
-            Text
+            hi
           </p>
         </div>
-        <div className='flex gap-10 justify-center flex-col'>
-          <p className='text-lg font-bold'>text</p>
+        <div className='md:w-fit flex gap-10 md:gap-52 justify-center md:justify-end flex-col md:flex-row'>
+          <p className='text-lg md:text-2xl font-bold'>Text</p>
           <Button
             className="text-black font-bold shadow-2xl shadow-amber-700"
             size="lg"
@@ -31,12 +31,12 @@ export default function HeroSection() {
             href="http://eepurl.com/iFDbkc"
             variant="solid"
           >
-            {btn}
+            Button
           </Button>
         </div>
       </div>
       <video className='absolute inset-0 object-cover w-full h-screen' autoPlay loop muted>
-        <source src={bg_url} type="video/mp4" />
+        <source src="#" type="video/mp4" />
       </video>
       <div className="absolute inset-0 bg-black opacity-50"></div>
     </div>
