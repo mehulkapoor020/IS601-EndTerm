4bbd33bfd366b321408c45088d76cdd59ef91669
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-15 22:41:01 -0500

    Integrate the CTA section closes RunjeethNikam#34

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import NavBar from '../../components/common-components/nav-bar';
 import Layout from "../../components/common-components/layout";
 import HeroSection from "../../components/home-page/hero-section";
 import { getDetails, getMenu, getTestimonials } from '../../lib/utils';
+import CTASection from "../../components/home-page/CTA";
 
 export default function Home() {
   const {content, data} = getDetails('landing_page/landing_page.md');
+  const ctaData = getDetails('landing_page/cta.md')
   return (
     <Layout>
       <NavBar />
@@ -15,6 +17,12 @@ export default function Home() {
           bg_url={data.bg_image}
           btn={data.btn}
           sub_heading={data.sub_heading}
+      />
+      <CTASection 
+        image={ctaData.data.image}
+        btn={ctaData.data.btn}
+        heading={ctaData.data.heading}
+        content={ctaData.content}
       />
     </Layout>
   )
