e693e26f76548d2ed7f5c6126840f60e75431eed
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-16 19:42:49 -0500

    Add text closes RunjeethNikam#64

--- components/about-us/buy-1-donate-1.tsx
+++ components/about-us/buy-1-donate-1.tsx
@@ -10,6 +10,16 @@ export default function Buy1Get1Section() {
         height={430}
         width={360}
       />
+      <div className='w-96 flex flex-col gap-10'>
+        <p
+          className='text-center font-bold text-4xl'
+        >
+          Our Mission
+        </p>
+        <p className='text-left text-lg'>
+          Our goal is to address hunger through a buy-1-give-1 model. For every item purchased, we commit to donating a corresponding item to the local food bank, contributing to the fight against hunger.
+        </p>
+      </div>
     </div>
   );
 };
