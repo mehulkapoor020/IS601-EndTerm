65fb861d1bda49ef2055b04379f0c00d063167d5
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-16 19:40:43 -0500

    Add the image to teh buy1donate1 component closes RunjeethNikam#63

--- components/about-us/buy-1-donate-1.tsx
+++ components/about-us/buy-1-donate-1.tsx
@@ -3,6 +3,13 @@ import Image from 'next/image';
 export default function Buy1Get1Section() {
   return (
     <div className="">
+      <Image
+        className='object-cover'
+        src="/about_us/get_1.png"
+        alt=""
+        height={430}
+        width={360}
+      />
     </div>
   );
 };
