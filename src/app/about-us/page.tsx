91e77db6bd5589130082502744a20bfc67013ff8
Runjeeth Nikam <nrunjeeth@gmail.com>
2023-12-16 21:18:08 -0500

    Integrate the Buy 1 donate component closes RunjeethNikam#65

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,12 +3,14 @@ import * as React from "react";
 import NavBar from '../../../components/common-components/nav-bar'
 import AboutUsSection from "../../../components/about-us/about-us";
 import Layout from "../../../components/common-components/layout";
+import Buy1Get1Section from "../../../components/about-us/buy-1-donate-1";
 
 export default async function AboutUs() {
   return (
     <Layout>
       <NavBar />
       <AboutUsSection />
+      <Buy1Get1Section />
     </Layout>
   )
 }
