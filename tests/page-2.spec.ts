82cf264e02377b50db8904b2918527f3d0f513f9
mehulkapoor020 <mehulkapoor020@gmail.com>
2023-12-18 04:28:13 -0500

    playwright test for Aboutus section subtext  closes #105

--- tests/page-2.spec.ts
+++ tests/page-2.spec.ts
@@ -22,4 +22,25 @@ test('Check About Us section for presence and content of heading', async ({ page
     // Assert that the actual content of the first <p> tag matches the expected content
     expect(firstParagraphText).toBe(expectedFirstParagraphContent);
   });
+
+  test('Check About Us section for presence and content of Subtext', async ({ page }) => {
+  // Navigate to the page where the component is rendered
+  await page.goto(websiteURL2); // Replace websiteURL with the actual URL
+
+  // Wait for the second <p> tag to be present in the DOM
+  await page.waitForSelector('#id-aboutUs-data p:nth-child(2)');
+
+  // Get the text content of the second <p> tag
+  const secondParagraphText = await page.$eval('#id-aboutUs-data p:nth-child(2)', (paragraph) => paragraph.textContent);
+
+  // Assert that the second <p> tag exists
+  expect(secondParagraphText).toBeTruthy();
+
+  // Replace the expected content with the actual text content you expect for the second <p> tag
+  const expectedSecondParagraphContent = 'We our an Authentic French Bakery. Our aim is to transport customers on a delightful journey to France through our renowned selection of delectable French pastries and culinary delights.';
+
+  // Assert that the actual content of the second <p> tag matches the expected content
+  expect(secondParagraphText).toBe(expectedSecondParagraphContent);
+});
+
   
