5790995c287bf45199446d2ddf58ed9d88e5a094
mehulkapoor020 <mehulkapoor020@gmail.com>
2023-12-18 03:36:46 -0500

    playwright test for herotext closes #90

--- tests/landing-page.spec.ts
+++ tests/landing-page.spec.ts
@@ -10,3 +10,20 @@ test('Check Web Page Title', async ({ page }) => {
   const title = await page.title();
   expect(title).toBe('Le Jardin');
 });
+
+test('Check hero text content', async ({ page }) => {
+    // Navigate to your home page
+    await page.goto(websiteURL);
+  
+  
+    // Wait for the div element to be present in the DOM
+    await page.waitForSelector('#idHero-text');
+  
+    
+   const elementExists = await page.$('#idHero-text');
+  
+   // Assert that the element with the specified id exists
+   expect(elementExists).toBeTruthy();
+  
+  });
+  
